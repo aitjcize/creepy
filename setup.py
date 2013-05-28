bcd79dcf2fcaaf77f23488e51b854bf594707b70
AZ Huang <aitjcize@gmail.com>
2013-05-28 14:47:50 +0800

    Bump version number.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
 	name = 'creepy',
-	version = '0.1.0',
+	version = '0.1.1',
 	description = 'Dead simple web crawler for Python',
 	author = 'Wei-Ning Huang (AZ)',
 	author_email = 'aitjcize@gmail.com',
