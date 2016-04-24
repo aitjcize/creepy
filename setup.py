4b233486cfecbb3b6647192253a5c4e20492161c
AZ Huang <aitjcize@gmail.com>
2016-04-24 15:05:46 +0800

    Bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -4,11 +4,18 @@ from distutils.core import setup
 
 setup(
 	name = 'creepy',
-	version = '0.1.5',
+	version = '0.1.6',
 	description = 'Dead simple web crawler for Python',
 	author = 'Wei-Ning Huang (AZ)',
 	author_email = 'aitjcize@gmail.com',
         url = 'http://github.com/Aitjcize/creepy',
 	license = 'GPL',
-        packages = ['creepy']
+        packages = ['creepy'],
+        classifiers = [
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        ],
 )
